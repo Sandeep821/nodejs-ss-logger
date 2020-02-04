7549a529d4df11f5430fad803f77c3e5743a89e1
Soni <Sandeep.Soni@vwcredit.com>
2020-02-04 16:59:30 -0600

    ading log sub methods

---
+++ src/logMethods.js
@@ -0,0 +1,22 @@
+import splunkLogger from '../index';
+import setLogFormatter from '../index'
+
+export class Logger {
+    static info(method, ...props) {
+        splunkLogger.info(setLogFormatter(method, 'info', ...props));
+    }
+    static error(method, ...params) {
+      splunkLogger.error(setLogFormatter(method, 'error', ...props));
+    }
+    static warn(method, ...params) {
+      splunkLogger.warn(setLogFormatter(method, 'warn', ...props));
+    }
+  
+    static trace(method, ...params) {
+      splunkLogger.trace(setLogFormatter(method, 'trace', ...props));
+    }
+
+    static debug(method, ...params) {
+        splunkLogger.debug(setLogFormatter(method, 'debug', ...props));
+      }
+  }
